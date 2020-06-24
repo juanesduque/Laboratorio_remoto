ee00681fe0e9ab137f5b766bf41e0927303349b6
Juanes <j.duque1@utp.edu.co>
2020-06-24 07:43:46 -0500

    Primer intento

---
+++ js/Aparecer.js
@@ -0,0 +1,7 @@
+var funcion = () => {
+	var div = document.getElementById("grafica")
+	div.setAttribute('class','col-md-12 center-block text-center')
+}
+
+var boton = document.getElementById('calcular')
+boton.addEventListener("click",funcion)
